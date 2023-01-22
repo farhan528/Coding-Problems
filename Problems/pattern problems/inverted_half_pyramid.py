4de4783cee0dbad05b5a32ba34f75e118715e3c0
Farhan Mustafa <43778434+farhan528@users.noreply.github.com>
2023-01-22 10:36:09 +0530

    Create inverted_half_pyramid.py

---
+++ Problems/pattern problems/inverted_half_pyramid.py
@@ -0,0 +1,13 @@
+# *****
+# ****
+# ***
+# **
+# *
+col = int(input())
+row = int(input())
+for i in range(row):
+    for j in range(col-i):
+        print("*", end="")
+    print("")
+            
+        
