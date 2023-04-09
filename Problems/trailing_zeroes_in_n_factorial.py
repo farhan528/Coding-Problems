880aeb085d71d9cd2072e9c1c6fd84f64d3d36ab
Farhan Mustafa <43778434+farhan528@users.noreply.github.com>
2023-04-09 16:35:47 +0530

    Update trailing_zeroes_in_n_factorial.py

--- Problems/trailing_zeroes_in_n_factorial.py
+++ Problems/trailing_zeroes_in_n_factorial.py
@@ -2,7 +2,7 @@
 
 # Intuition:
 #   For any given number n, the no of trailing zeroes in n! would be dependent upon the number of 10s or 2s x 5s are there. And for any positive integer it is a general
-#   obervation that the primt factor of any number will have occurences of 2s > 5s. So, basically we will calculate only the occurences of 5s because those many 5s will
+#   obervation that the prime factor of any number will have occurences of 2s > 5s. So, basically we will calculate only the occurences of 5s because those many 5s will
 #   be paired with 2s to form 10. To calculate number of 5s in any given number we can use legendre's formula in which we keep on dividing n by 5 ** k, where 1<=k and
 #   k goes until 5 // 5**k isn't 0
 
